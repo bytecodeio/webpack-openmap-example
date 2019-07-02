f19b67f2b635899f5deef6ffaf96b918b0c6faed
Matthew Dalby <matthew.dalby@bytecode.io>
2019-07-02 13:06:49 -0700

    Working baseline from example

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { Greeter } from './greeter';
+import {OpenMapApi} from './openMapApi';
 
 const g = new Greeter('Juri');
 g.greet();
+
+const mapApi = new OpenMapApi();
+mapApi.setMapInstance("mapHolder");
--- src/openMapApi.ts
+++ src/openMapApi.ts
@@ -1,13 +1,19 @@
 //var osm: any = require('osm');
-var ol: any = require('ol');
+//var ol: any = require('ol');
 //import {Map, View} from 'ol';
 //import {fromLonLat} from 'ol/proj';
 //var MapView: any = require('ol/View');
-var TileLayer: any = require('ol/layer/Tile');
+//var TileLayer: any = require('ol/layer/Tile');
 //var OSM: any = require('ol/source/OSM');
-var coordinateFunctions = require('ol/coordinate');
+//var coordinateFunctions = require('ol/coordinate');
 
-console.debug( require('ol/source/OSM'));
+import TileLayer from "ol/layer/tile";
+import Map from "ol/map";
+import XYZ from "ol/source/xyz";
+import View from "ol/view";
+
+
+//console.debug( require('ol/source/OSM'));
 
 export class OpenMapApi{
     makeSampleCall():string{ 
@@ -21,6 +27,24 @@ export class OpenMapApi{
         
         console.log('Initializing map in div element \''+ targetDomElement +'\'');
 
+      var mapCfg = {
+        target: targetDomElement,
+        view: new View({
+            center: [0, 0],
+            zoom: 2,
+        }),
+        layers: [
+          new TileLayer({
+              source: new XYZ({
+                  url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              }),
+          }),
+        ],  
+      };
+
+        const map = new Map(mapCfg);
+
+        /*
         var map = new ol.Map({
             view: new ol.View({
               center: [0, 0],
@@ -33,6 +57,6 @@ export class OpenMapApi{
             ],
             target: targetDomElement
           });
-
+        */
     }
 }
